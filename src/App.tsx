4c2614b4c7237da10ec31e6137224d1f730fc47b
Donovan Messer <donovanmess01@gmail.com>
2023-02-15 13:08:22 -0500

    testing somrthing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import lefish from "./augh.jpg";
+import lefish from "./images/augh.jpg";
 
 function App(): JSX.Element {
     return (
